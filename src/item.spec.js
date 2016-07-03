63bb750a430e57c26089bcc42773e583d1e7c937
Reiss Jarvis <reissefer@gmail.com>
2016-07-03 12:59:09 +0100

    fix mapped files for tests

--- src/item.spec.js
+++ src/item.spec.js
@@ -3,10 +3,6 @@ describe("Item Tests", function () {
     it("Has Name", function () {
         var item = { name: "Test Name" };
         expect(item.name).toBe("Test Name");
-    }),
-        it("Has Name", function () {
-            var item = { name: "Test Name" };
-            expect(item.name).toBe("fail");
-        });
+    });
 });
 //# sourceMappingURL=item.spec.js.map
